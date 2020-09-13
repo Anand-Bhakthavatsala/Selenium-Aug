6f31ee16358f21459c5c5962943a8d449028d3e8
Anand <Anand@DESKTOP-H21MM63>
2020-09-13 11:52:35 +0530

    Added one print to chess

--- Selenium-Aug/src/Java/Chess.java
+++ Selenium-Aug/src/Java/Chess.java
@@ -16,6 +16,7 @@ public class Chess extends Sports{
 
 	void chess() { //method overrding
 		// TODO Auto-generated method stub
+		System.out.println("Improve logical thinking");
 		System.out.println("V Anand v/s Kasparov");
 		System.out.println("India and Russia are declared as joint winners of 2020 b'cas of server failure");
 	}
